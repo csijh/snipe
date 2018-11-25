4d825d051f5fb1f3fc155c2f51684fd4fca56b0b
Ian Holyer <csijh@bristol.ac.uk>
2018-11-25 22:41:18 +0000

    catchup

---
+++ src/out.c
@@ -0,0 +1,27 @@
+void f() {
+    for (int i=0; i<n; i++) {
+        i = i;
+    }
+}
+
+void f()
+{
+    for (int i=0; i<n; i++)
+    {
+        i = i;
+    }
+}
+
+void f()
+{ for (int i=0; i<n; i++)
+    { i = i;
+        i = i;
+    }
+}
+
+void f() {
+    for (int i=0; i<n; i++) {
+        i = i; } }
+    
+    f(x,
+        y);
